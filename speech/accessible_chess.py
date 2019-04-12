e5bc11bf079465648c54c0d906277e0ce6549e9d
david-thornton <thornton.da@husky.neu.edu>
2019-04-12 14:49:57 -0400

    New hotword (#8)

--- speech/accessible_chess.py
+++ speech/accessible_chess.py
@@ -12,11 +12,9 @@ def main(comm):
 		while True:
 			if queue.message_count() > 0:
 				print("New message: '{}'".format(queue.pop_message()))
-	
+
 	thrd = threading.Thread(target=show_messages)
 	thrd.start()
-	snowboy_umdl = os.path.join(os.path.split(os.path.realpath(__file__))[0], "resources/snowboy.umdl")
+	snowboy_umdl = os.path.join(os.path.split(os.path.realpath(__file__))[0], "resources/Gambit.pmdl")
 	detector = snowboydecoder.HotwordDetector(snowboy_umdl, sensitivity=0.5, audio_gain=1)
 	detector.start(partial(speech.main, detector, comm))
-	
-
